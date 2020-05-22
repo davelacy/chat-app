7d147ca7b65e9c8cfe70c52673991beac6500637
Dave Lacy <wdlacy@gmail.com>
2020-05-22 16:29:23 -0700

    added chalk

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const http = require("http");
 const app = express();
+const chalk = require("chalk");
 
 const ngrok = require("ngrok");
 const path = require("path");
@@ -16,12 +17,16 @@ app.get("/", function (req, res) {
   res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
+server.listen(port, () =>
+  console.log(`Local chat app running on: http://localhost:${port}`)
+);
+
 (async function () {
   const url = await ngrok.connect(port);
-  console.log(`Public shareable link can be found here: ${url}`);
+  console.log(chalk.bold.yellow("=".repeat(50)));
+  console.log(chalk.bold.blue(`Public shareable link: ${url}`));
+  console.log(chalk.bold.yellow("=".repeat(50)));
 })();
-
-server.listen(port, () => console.log(`Web users listening on port: ${port}`));
 
 const stop = () => server.close();
 
