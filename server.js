ca9634cf60e3db3ba3472428107ffeb8ac456a2b
Dave Lacy <wdlacy@gmail.com>
2020-05-22 16:45:33 -0700

    removed unused packages

--- server.js
+++ server.js
@@ -1,9 +1,7 @@
 const express = require("express");
 const http = require("http");
 const app = express();
-const chalk = require("chalk");
 
-const ngrok = require("ngrok");
 const path = require("path");
 const port = process.env.PORT || 9000;
 const server = http.createServer(app);
@@ -18,15 +16,8 @@ app.get("/", function (req, res) {
 });
 
 server.listen(port, () =>
-  console.log(`Local chat app running on: http://localhost:${port}`)
+  console.log(`Local chat app running at: http://localhost:${port}`)
 );
-
-(async function () {
-  const url = await ngrok.connect(port);
-  console.log(chalk.bold.yellow("=".repeat(50)));
-  console.log(chalk.bold.blue(`Public shareable link: ${url}`));
-  console.log(chalk.bold.yellow("=".repeat(50)));
-})();
 
 const stop = () => server.close();
 
